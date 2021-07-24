4af1e8b9907ab8a842113d909f2e03a8f679cee5
2y2son4 <adrianadelteso@gmail.com>
2021-07-24 12:11:21 +0200

    :sparkles: Add helloworld route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// var locationsRouter = require('./routes/locations');
 
 var app = express();
 
@@ -17,18 +18,22 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+/* to serve static objects from the /public/ dir, but to make them actually seem like they're coming from the top level. For example, the images directory is C:\node\nodetest1\public\images … but it is accessed at http://localhost:3000/images*/
 app.use(express.static(path.join(__dirname, 'public')));
 
+/*This is telling Express that requests to http://localhost:3000/ should use the index router ('./routes/index'), and requests to http://localhost:3000/users shold use the users router file. */
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// app.use('/locations', locationsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
